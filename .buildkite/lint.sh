47178dc5c5e387e36e5115419754b46c0e8fe608
Richard van der Hoff <richard@matrix.org>
2020-06-04 12:53:08 +0100

    clean up lint scripts

--- .buildkite/lint.sh
+++ .buildkite/lint.sh
@@ -5,6 +5,7 @@ set -e
 cd `dirname $0`/..
 
 go get golang.org/x/lint/golint
+go install golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
 go get github.com/fzipp/gocyclo
 
-exec ./hooks/pre-commit
+./scripts/lint.sh
