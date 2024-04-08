e4c7e3fdcff784950e8686de93a6ab02d113aa4e
Tulir Asokan <tulir@maunium.net>
2024-04-08 13:40:09 +0300

    Add even more hacks for googleplaytester

--- submit.go
+++ submit.go
@@ -369,7 +369,7 @@ func (s *submitServer) parseRequest(w http.ResponseWriter, req *http.Request, re
 		}
 	}
 
-	if strings.TrimSpace(p.UserText) == "googleplaytester2" {
+	if strings.HasPrefix(strings.TrimSpace(p.UserText), "googleplaytester2") {
 		log.Println("Dropping report with title googleplaytester2")
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
